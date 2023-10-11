e18a1df8a63bb0c091a05db95a0010b1e081f928
sheaDurgin <shea.durgin@maine.edu>
2023-10-11 00:09:52 -0400

    updated

--- my_nestris.py
+++ my_nestris.py
@@ -74,23 +74,80 @@ shape_orientations = {
     's':
     [
         '0000001101100000',
-        '0100011000100000',
+        '0000001000110001',
     ],
     'z':
     [
         '0000011000110000',
-        '0010011001000000',
+        '0000000100110010',
     ]
 }
 
 colors = {
-    'i': (255,255,255),
-    'o': (0, 0, 255),
-    't': (255, 0, 0),
-    'l': (0, 0, 255),
-    'j': (255, 0, 0),
-    's': (255,255,255),
-    'z': (255,255,255),
+    'i': (200,200,255),
+    'o': (200,200,255),
+    't': (200,200,255),
+    'l': (255, 0, 0),
+    'j': (0, 0, 255),
+    's': (0,0,255),
+    'z': (255,0,0),
+}
+
+frames = {
+    0: 48,
+    1: 43,
+    2: 38,
+    3: 33,
+    4: 28,
+    5: 23,
+    6: 18,
+    7: 13,
+    8: 8,
+    9: 6,
+    10: 5,
+    11: 5,
+    12: 5,
+    13: 4,
+    14: 4,
+    15: 4,
+    16: 3,
+    17: 3,
+    18: 3,
+    19: 2,
+    29: 1
+}
+
+level_delay = {
+    0: 10,
+    1: 20,
+    2: 30,
+    3: 40,
+    4: 50,
+    5: 60,
+    6: 70,
+    7: 80,
+    8: 90,
+    9: 100,
+    10: 100,
+    11: 100,
+    12: 100,
+    13: 100,
+    14: 100,
+    15: 100,
+    16: 110,
+    17: 120,
+    18: 130,
+    19: 140,
+    20: 150,
+    21: 160,
+    22: 170,
+    23: 180,
+    24: 190,
+    25: 200,
+    26: 200,
+    27: 200,
+    28: 200,
+    29: 200
 }
 
 def get_new_letter(prev_piece_index):
@@ -105,6 +162,8 @@ def update_placement(piece, color):
         if block == '1':
             col_offset, row_offset = offsets[idx]
             spot = (piece.col + col_offset, piece.row + row_offset)
+            if spot[1] >= TOTAL_ROWS:
+                continue
             board.blocks[spot] = color
 
 def check_and_update_placement(piece, before_piece):
@@ -120,7 +179,11 @@ def check_and_update_placement(piece, before_piece):
             col_offset, row_offset = offsets[idx]
             spot = (piece.col + col_offset, piece.row + row_offset)
             
-            if spot[0] >= TOTAL_COLS or spot[0] < 0 or spot[1] < 0 or spot[1] >= TOTAL_ROWS or (board.blocks[spot] != (0, 0, 0) and spot not in seen_spots):
+            if spot[0] >= TOTAL_COLS or spot[0] < 0 or spot[1] < 0:
+                return False
+            if spot[1] >= TOTAL_ROWS:
+                continue
+            if board.blocks[spot] != (0, 0, 0) and spot not in seen_spots:
                 return False
             
     update_placement(before_piece, (0, 0, 0))
@@ -164,6 +227,8 @@ class Piece:
   
     # direction is 1 (right) or -1 (left)
     def move_sideways(self, direction):
+        if not self.can_move:
+            return
         before_piece = copy.deepcopy(self)
         self.col += direction
         if not check_and_update_placement(self, before_piece):
@@ -183,18 +248,13 @@ class Piece:
 
 
 class Board:
-    def __init__(self):
+    def __init__(self, level):
         self.blocks = {(col, row): (0, 0, 0) for col in range(10) for row in range(20)}
+        self.score = 0
+        self.level = level
+        self.lines_cleared = 0
+        self.lines_until_level_change = level_delay[self.level]
         
-        # time in seconds that a piece falls one block
-        self.gravity = 0.2
-    
-    def increase_gravity(self):
-        if self.gravity >= 0.02:
-            self.gravity -= 0.01
-        else:
-            self.gravity /= 2
-
     def clear_lines(self):
         rows_cleared = [False for _ in range(TOTAL_ROWS)]
         for row in range(TOTAL_ROWS):
@@ -218,20 +278,20 @@ class Board:
             for col in range(TOTAL_COLS):
                 self.blocks[(col, row - offset)] = self.blocks[(col, row)]
                 self.blocks[(col, row)] = (0, 0, 0)
-
-board = Board()
+        
+        return offset
 
 # pygame setup
 pygame.init()
 
-screen_length = 800
+screen_width = 800
 screen_height = 900
-screen = pygame.display.set_mode((screen_length, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 grid_length = 400
 grid_height = 800
 
-top_left_x = (screen_length - grid_length) // 2
+top_left_x = (screen_width - grid_length) // 2
 top_left_y = screen_height - grid_height
 
 cell_size = 40
@@ -251,8 +311,109 @@ def draw_border():
     # Draw the border rectangle
     pygame.draw.rect(screen, (255, 255, 255), border_rect, 1)
 
-def calculate_points(level, lines_cleared):
+NEXT_PIECE_X = 500
+NEXT_PIECE_Y = 1065
+
+def display_next_piece(piece):
+    # Clear the area where the next piece will be displayed
+    pygame.draw.rect(screen, (0, 0, 0), (NEXT_PIECE_X + 110, NEXT_PIECE_Y - 665, 200, 100))
     
+    # Draw the next piece
+    for idx, block in enumerate(piece.orientation):
+        if block == '1':
+            col_offset, row_offset = offsets[idx]
+            spot = (piece.col + col_offset, piece.row + row_offset)
+            pygame.draw.rect(screen, piece.color, (NEXT_PIECE_X + spot[0] * cell_size, NEXT_PIECE_Y + 100 - (spot[1] * cell_size), cell_size, cell_size))
+
+def display_lines_cleared():
+    font = pygame.font.Font(None, 36)
+    text = font.render(f"Lines: {board.lines_cleared}", True, (255, 255, 255))
+    text_rect = text.get_rect()
+    text_rect.center = (screen_width // 4, 50)
+
+    pygame.draw.rect(screen, (0, 0, 0), (text_rect.left, text_rect.top, text_rect.width, text_rect.height))
+
+    screen.blit(text, text_rect)
+
+def display_level():
+    font = pygame.font.Font(None, 36)
+    text = font.render(f"Level: {board.level}", True, (255, 255, 255))
+    text_rect = text.get_rect()
+    text_rect.center = (screen_width // 2, 50)
+
+    pygame.draw.rect(screen, (0, 0, 0), (text_rect.left, text_rect.top, text_rect.width, text_rect.height))
+
+    screen.blit(text, text_rect)
+
+def display_score():
+    font = pygame.font.Font(None, 36)
+    text = font.render(f"Score: {board.score}", True, (255, 255, 255))
+    text_rect = text.get_rect()
+    text_rect.center = (screen_width // 1.3, 50)
+
+    pygame.draw.rect(screen, (0, 0, 0), (text_rect.left, text_rect.top, text_rect.width, text_rect.height))
+
+    screen.blit(text, text_rect)
+
+score_per_line_cleared = [40, 100, 300, 1200, 0]
+
+def calculate_points(lines_cleared):
+    board.lines_cleared += lines_cleared
+
+    if board.lines_until_level_change <= board.lines_cleared:
+        board.lines_until_level_change += 10
+        board.level += 1
+
+    return score_per_line_cleared[lines_cleared - 1] * (board.level + 1)
+
+def select_level():
+    pygame.init()
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Select Level")
+
+    selected_level = 0  # Initial selected level
+    levels = 30  # Total number of levels
+    font = pygame.font.Font(None, 36)  # Font for displaying the level
+    text_color = (255, 255, 255)  # White text color
+    background_color = (0, 0, 0)  # Black background color
+
+    key_delay = 125  # Delay in milliseconds between key presses
+    key_timer = 0
+
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        keys = pygame.key.get_pressed()
+
+        key_timer += clock.tick()
+
+        if key_timer >= key_delay:
+            if keys[pygame.K_j]:
+                selected_level = (selected_level - 1) % levels
+                key_timer = 0  # Reset the key timer
+
+            if keys[pygame.K_l]:
+                selected_level = (selected_level + 1) % levels
+                key_timer = 0  # Reset the key timer
+
+        if keys[pygame.K_RETURN]:
+            return selected_level
+
+        # Clear the screen
+        screen.fill(background_color)
+
+        # Display the selected level
+        text = font.render(f"Selected Level: {selected_level}", True, text_color)
+        text_rect = text.get_rect(center=(screen_width // 2, screen_height // 2))
+        screen.blit(text, text_rect)
+
+        pygame.display.update()
+
+board = Board(select_level())
 
 def main():
     screen.fill("black")
@@ -262,6 +423,7 @@ def main():
     curr_piece = Piece(-1)
     update_placement(curr_piece, curr_piece.color)
     next_piece = Piece(curr_piece.letter_index)
+    display_next_piece(next_piece)
 
     fall_time = 0
 
@@ -269,8 +431,9 @@ def main():
     dt = 0
 
     draw_border()
-
-    score = 0
+    display_score()
+    display_lines_cleared()
+    display_level()
 
     SHIFT_DELAY = 16  # Initial delay before repeating the sideways move
     SHIFT_INTERVAL = 5  # Interval between repeated sideways moves
@@ -289,9 +452,13 @@ def main():
         # Get the elapsed time in seconds since the last frame
         dt = clock.tick(60) / 1000
 
-        # PIECE FALLING CODE
-        fall_time += dt  # Use dt to control the falling speed
-        if fall_time >= board.gravity:
+        fall_time += dt
+        frames_index = board.level
+        if frames_index >= 29:
+            frames_index = 29
+        elif frames_index >= 19:
+            frames_index = 19
+        if fall_time >= (1.0 / 60) * frames[frames_index] * 3:  # 1.0/60 represents 1 frame at 60 FPS
             fall_time = 0
             curr_piece.move_down()
 
@@ -340,13 +507,20 @@ def main():
                 curr_piece.move_sideways(LEFT)
             if is_l_pressed:
                 curr_piece.move_sideways(RIGHT)
-            board.clear_lines()
+            board.score += calculate_points(board.clear_lines())
+
+            display_score()
+            display_lines_cleared()
+            display_level()
+
             speedup = False
             curr_piece = next_piece
             if check_loss(curr_piece):
                 running = False
             update_placement(curr_piece, curr_piece.color)
             next_piece = Piece(curr_piece.letter_index)
+            # IMPLEMENT NEXT PIECE SCREEN
+            display_next_piece(next_piece)
 
         # Handle sideways autoshift
         if current_shift_delay > 0:
@@ -362,7 +536,7 @@ def main():
             current_shift_interval -= 1
 
         if speedup:
-            fall_time += dt * 4  # Increase falling speed when "K" key is held down
+            fall_time += dt * (100 - board.level * 2)
         else:
             fall_time += dt
 
@@ -375,6 +549,5 @@ def main():
                 pygame.display.quit()
                 print("QUIT")
                 quit()
-
 
 main()
